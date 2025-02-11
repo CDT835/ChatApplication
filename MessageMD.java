ad16890f9b7e5ae61282cd3f9d6355bd6af555b9
CDT835 <chuductien5201314@gmail.com>
2025-02-11 19:39:27 +0700

    Add files via upload

---
+++ MessageMD.java
@@ -0,0 +1,38 @@
+package chatapplication;
+
+import java.sql.Timestamp;
+
+public class MessageMD {
+    private int id;
+    private final String sender;
+    private final String content;
+    private final Timestamp timestamp;
+
+    // Constructor
+    public MessageMD(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+}
